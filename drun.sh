7376511fe302edee397349181aa7859b395458f4
David Capino <david.capino@gmail.com>
2019-07-11 12:45:44 -0700

    buster

--- drun.sh
+++ drun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v ~/.gitconfig:/root/.gitconfig -v /var/run/docker.sock:/var/run/docker.sock -v ~/source:/Users/x37y/source  -v ~/.ssh:/root/.ssh -v ~/.aws:/root/.aws -it debiandev bash
+docker run -v ~/.gitconfig:/root/.gitconfig -v /var/run/docker.sock:/var/run/docker.sock -v ~/source:/root/source -v ~/.ssh:/root/.ssh -v ~/.aws:/root/.aws -it debiandev bash
